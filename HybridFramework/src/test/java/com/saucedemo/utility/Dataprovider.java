d438cad915fd77c2a5c8f258bd08e3aa736622c4
victorvijay1210 <70469583+victorvijay1210@users.noreply.github.com>
2023-10-28 13:21:40 +0530

    new change

--- HybridFramework/src/test/java/com/saucedemo/utility/Dataprovider.java
+++ HybridFramework/src/test/java/com/saucedemo/utility/Dataprovider.java
@@ -1,48 +1,49 @@
-package com.saucedemo.utility;
-
-import java.io.File;
-import java.io.FileInputStream;
-
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-public class Dataprovider {
-	
-	XSSFWorkbook wb;
-	
-	public Dataprovider() {
-		
-		File src = new File("C:\\Users\\vijay\\eclipse-workspace\\HybridFramework\\TestData\\TestData.xlsx");
-		
-		try {
-			FileInputStream fi = new FileInputStream(src);
-			
-			 wb = new XSSFWorkbook(fi);
-		}
-		catch (Exception e) {
-			System.out.println("unable to read excel" + e);
-		}
-			
-		
-		 
-		 
-	}
-	
-public String getStringData(int sheetindex, int row, int coloumn) {
-		
-		return wb.getSheetAt(sheetindex).getRow(row).getCell(coloumn).getStringCellValue();
-		
-	}
-
-	public String getStringData(String Sheetname, int row, int coloumn) {
-		
-		return wb.getSheet(Sheetname).getRow(row).getCell(coloumn).getStringCellValue();
-		
-	}
-	
-public double getNumericData(String Sheetname, int row, int coloumn) {
-		
-		return wb.getSheet(Sheetname).getRow(row).getCell(coloumn).getNumericCellValue();
-		
-	}
-	
-	}
+//package com.saucedemo.utility;
+//
+//import java.io.File;
+//import java.io.FileInputStream;
+//
+//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+//
+//public class Dataprovider {
+//	
+//	
+//	XSSFWorkbook wb;
+//	
+//	public Dataprovider() {
+//		
+//		File src = new File("C:\\Users\\vijay\\eclipse-workspace\\HybridFramework\\TestData\\TestData.xlsx");
+//		
+//		try {
+//			FileInputStream fi = new FileInputStream(src);
+//			
+//			 wb = new XSSFWorkbook(fi);
+//		}
+//		catch (Exception e) {
+//			System.out.println("unable to read excel" + e);
+//		}
+//			
+//		
+//		 
+//		 
+//	}
+//	
+//public String getStringData(int sheetindex, int row, int coloumn) {
+//		
+//		return wb.getSheetAt(sheetindex).getRow(row).getCell(coloumn).getStringCellValue();
+//		
+//	}
+//
+//	public String getStringData(String Sheetname, int row, int coloumn) {
+//		
+//		return wb.getSheet(Sheetname).getRow(row).getCell(coloumn).getStringCellValue();
+//		
+//	}
+//	
+//public double getNumericData(String Sheetname, int row, int coloumn) {
+//		
+//		return wb.getSheet(Sheetname).getRow(row).getCell(coloumn).getNumericCellValue();
+//		
+//	}
+//	
+//	}
